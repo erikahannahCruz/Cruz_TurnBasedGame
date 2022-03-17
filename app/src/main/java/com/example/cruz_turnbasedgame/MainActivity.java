f7107706ded3a4c64e4538f6f3c8e6600339c0c9
p1p1pupu <11erikacruz11@gmail.com>
2022-03-17 22:32:35 +0800

    Added Increase Damage ability

--- app/src/main/java/com/example/cruz_turnbasedgame/MainActivity.java
+++ app/src/main/java/com/example/cruz_turnbasedgame/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //stuff changed to hide title banner upon startup
+        //Hides title banner upon startup
         requestWindowFeature(Window.FEATURE_NO_TITLE);
             getSupportActionBar().hide();
             this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
